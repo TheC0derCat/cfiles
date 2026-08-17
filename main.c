cbe8eea219fecfabb549beaf8a9dc092161b578a
Silly Cat <CatC0der@proton.me>
2026-08-17 13:43:56 -0700

    added listing functionality

--- main.c
+++ main.c
@@ -1,17 +1,22 @@
 #include<ncurses.h>
+#include<dirent.h>
 int main(void){
 	initscr();
 	raw();
 	noecho();
 	keypad(stdscr, true);
 	while(1){
+		struct dirent *de;
+		DIR *dr = opendir(".");
 		erase();
-		printw("Hello, world!");
+		while ((de = readdir(dr)) != NULL)
+			printw("%s\n", de->d_name);
 		int input = getch();
 		switch(input){
 			case 'q':
 				endwin();
 				return 0;
 		}
+		closedir(dr);
 	}
 }
