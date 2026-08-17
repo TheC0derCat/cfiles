916f1e65cd487617b32ec489498260b6dbe70913
Silly Cat <CatC0der@proton.me>
2026-08-17 13:54:42 -0700

    added cursor movement

--- main.c
+++ main.c
@@ -5,17 +5,21 @@ int main(void){
 	raw();
 	noecho();
 	keypad(stdscr, true);
+	int cursor = 0;
 	while(1){
 		struct dirent *de;
 		DIR *dr = opendir(".");
 		erase();
-		while ((de = readdir(dr)) != NULL)
-			printw("%s\n", de->d_name);
-		int input = getch();
+		int i = 0;
+		while ((de = readdir(dr)) != NULL){
+			printw("%c%s\n", ((i == cursor) ? '=' : ' '),de->d_name);
+			i++;
+		}
+		char input = getch();
 		switch(input){
-			case 'q':
-				endwin();
-				return 0;
+			case 'q': endwin(); return 0;
+			case 'j': cursor++; break;
+			case 'k': if(cursor > 0) cursor--; break;
 		}
 		closedir(dr);
 	}
